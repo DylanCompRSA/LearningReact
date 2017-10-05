5c926c69063fa87d2a1e88838d2e2c02cc638b50
DylanCompRSA <dylan@comprsa.com>
2017-10-05 14:53:40 +0200

    routing, dependencies, glamorous

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {App,Header,Container,Home,About,Contact} from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div className="App container"><Header/></div>, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
